781a04bf852368ece1e4358a8e12ad42dabb5dab
Rebekah Cruz <rebekah.cruz702@gmail.com>
2024-02-03 13:00:20 -0600

    update week 1 and crate folder for week 2

--- Cohort5/Week1/challenges/week1.js
+++ Cohort5/Week1/challenges/week1.js
@@ -2,18 +2,44 @@
 document.addEventListener('DOMContentLoaded', function () {
     // Challenge 1: Declare a variable and assign it a string value
     // Your code here
-
+    var firstName = 'name'; // old way to do things
+    let lastName = 'lastName'; // can change
+    const fullName = 'name lastName'; // constant variable, does not change
     // Challenge 2: Create a function that logs a message to the console
     // Your code here
+    let showMessage = () => console.log('this is a message from the console!')
+    let showMessageAgain = () => {
+        showMessage();
+        showMessage();
+    }
+    //arrow functions have implicit return
+    // if you are just doing one thing, you dont need the curly braces or 'return'
+
+    function showOtherMessage (name) {
+        return console.log(`this is another message from ${name}`)
+    }
 
     // Challenge 3: Call the function created in Challenge 2
     // Your code here
+    showMessage();
+    showMessageAgain()
+    showOtherMessage('Bekah');
 
     // Challenge 4: Select an element by its ID and update its content
     // Your code here
+    let heading = document.getElementById('heading')
+    heading.innerText = 'This is a heading!'
 
     // Challenge 5: Add an event listener to the button on your page that triggers a an alert with a welcome message using the inputted name.
     // Your code here
+    let nameBox = document.getElementById('fname');
+    let button = document.querySelector('button');
+    button.addEventListener('click', function(event) {
+        event.preventDefault()
+        console.log(nameBox.value)
+        console.log(event.target.value)
+    })
+
 
     // Challenge 6: Use event.target to access the clicked element
     // Your code here
@@ -61,6 +87,8 @@ document.addEventListener('DOMContentLoaded', function () {
     // Your code here
 
     // Challenge 21: Load this JS file on your HTML page.
+    // using the script tag and loading this file
+    // <script src='./week1.js'></script>
 
     // Challenge 22: Move your CSS styles into an external file and load it on your HTML page.
 });
